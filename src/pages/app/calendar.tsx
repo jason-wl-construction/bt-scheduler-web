2d0244848d3d97ec4aaf97aca6f38fb540c8ee54
jason-wl-construction <jason@wl-construction.com>
2025-11-04 15:51:15 -0800

    Update calendar.tsx

--- src/pages/app/calendar.tsx
+++ src/pages/app/calendar.tsx
@@ -2,11 +2,11 @@ import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 import { fetchEvents } from '../../utils/api'
 
-// After installing FullCalendar, uncomment below:
-// const FullCalendar = dynamic(() => import('@fullcalendar/react'), { ssr: false });
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// import timeGridPlugin from '@fullcalendar/timegrid'
-// import interactionPlugin from '@fullcalendar/interaction'
+// Enable FullCalendar
+const FullCalendar = dynamic(() => import('@fullcalendar/react'), { ssr: false });
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 export default function CalendarPage() {
   const [events, setEvents] = useState<any[]>([])
@@ -18,8 +18,7 @@ export default function CalendarPage() {
   return (
     <main style={{ padding: 16 }}>
       <h1>Calendar</h1>
-      <p>(Install FullCalendar and uncomment code to enable the calendar UI.)</p>
-      {/*
+
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
@@ -30,7 +29,8 @@ export default function CalendarPage() {
         }}
         events={events}
       />
-      */}
+
+      {/* debug preview of first items */}
       <pre>{JSON.stringify(events.slice(0,5), null, 2)}</pre>
     </main>
   )
