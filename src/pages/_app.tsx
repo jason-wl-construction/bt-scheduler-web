5caf028abc3e0f3b9cae7839c15b004fa62ac0d2
jason-wl-construction <jason@wl-construction.com>
2025-11-05 07:44:19 -0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,3 @@
-import '@fullcalendar/core/index.css'
-import '@fullcalendar/daygrid/index.css'
-import '@fullcalendar/timegrid/index.css'
+import '@fullcalendar/core/main.css'
+import '@fullcalendar/daygrid/main.css'
+import '@fullcalendar/timegrid/main.css'
